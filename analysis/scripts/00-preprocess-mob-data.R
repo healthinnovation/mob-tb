2c2e413ff760c9276f23f703f98131c9f57cd10e
Diego Villa <ddevildiego@gmail.com>
2022-03-17 17:05:32 -0500

    mob-tb dataset

--- analysis/scripts/00-preprocess-mob-data.R
+++ analysis/scripts/00-preprocess-mob-data.R
@@ -15,13 +15,9 @@ mob_od_matrix <- get_od_data(mob_raw, wide = TRUE)
 #TODO: Add argument in get_od_data to choose which column is origin and which
 # is destination
 
-lima_metro <- c("LIMA", "CALLAO")
-
 mob_lima_metro_od <-
   mob_od %>%
-  filter(
-    dept_ori %in% lima_metro, dept_des %in% lima_metro
-  )
+  filter(prov_ori == "LIMA", prov_des == "LIMA")
 
 names(mob_lima_metro_od) <- c(
   "ubigeo_des", "ubigeo_ori", "cases", "dept_des", "prov_des", "distr_des",
@@ -30,10 +26,7 @@ names(mob_lima_metro_od) <- c(
 
 mob_lima_metro_od <-
   mob_lima_metro_od %>%
-  select(
-    ubigeo_ori, ubigeo_des, cases, dept_ori, prov_ori, distr_ori, dept_des,
-    prov_des, distr_des
-  ) %>%
+  select(ubigeo_ori, ubigeo_des, cases) %>%
   # arrange(dept_ori, prov_ori, distr_ori, dept_des, prov_des, distr_des)
   arrange(ubigeo_ori, ubigeo_des)
 
