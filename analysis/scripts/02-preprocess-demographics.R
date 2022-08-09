2f8447a9ee2452c84ec26924cfc6ba1b2fe34bcc
Diego Villa <ddevildiego@gmail.com>
2022-08-09 13:08:41 -0500

    Update demographics

--- analysis/scripts/02-preprocess-demographics.R
+++ analysis/scripts/02-preprocess-demographics.R
@@ -1,18 +1,50 @@
 library(fs)
 library(readr)
 library(dplyr)
-library(stringi)
 
-raw_path <- path("analysis", "data", "raw")
-infile <- "per-demography-2017.csv"
-infile_path <- path(raw_path, infile)
-demo_raw <- read_csv(infile_path)
+raw_path <- path("analysis/data/raw/")
+demographics_filename <- "district-demographics.csv"
+demographics_filepath <- path(raw_path, demographics_infilename)
+demographics_raw <- read_csv(
+  demographics_filepath, col_types = paste(c("cccc", rep("d", 14)), collapse = "")
+)
 
-demo <- demo_raw %>%
+population_filename <- "district-population-census-2017.csv"
+population_filepath <- path(raw_path, population_infilename)
+population <- read_csv(
+  population_filepath, col_select = c("ubigeo", "population"), col_types = "cd"
+)
+
+demographics_full <- demographics_raw %>%
+  inner_join(population, by = "ubigeo") %>%
+  select(ubigeo:district, population, everything()) %>%
   mutate(
-    across(
-      where(is.character), ~ stri_trans_general(toupper(.), id = "Latin-ASCII")
-    )
+    life_expectancy_norm = (life_expectancy - 25) / (85 - 25),
+    years_edu_norm = (years_edu - 1.8) / (16 - 1.8),
+    edu_achievement = (life_expectancy_norm * years_edu_norm) ^ 0.5,
+    family_income_norm = (family_income - 35) / (2500 - 35),
+    hh_1_nbi_or_more_prop = hh_1_nbi_or_more / number_hh,
+    hh_inadequate_char_prop = hh_inadequate_char / number_hh,
+    overcrowded_hh_prop = overcrowded_hh / number_hh,
+    hh_wo_sanitation_prop = hh_wo_sanitation / number_hh,
+    hh_school_absence_prop = hh_school_absence / number_hh,
+    hh_high_economic_dependence_prop = hh_high_economic_dependence / number_hh
   )
 
-readRDS("analysis/data/raw/indic_dist_censo_2017.rds")
+demographics <- demographics_full %>%
+  filter(province == "LIMA") %>%
+  select(
+    ubigeo:monetary_poverty, life_expectancy = life_expectancy_norm,
+    complete_secondary_edu, years_edu = years_edu_norm,
+    family_income = family_income_norm, hh_1_nbi_or_more = hh_1_nbi_or_more_prop,
+    hh_inadequate_char = hh_inadequate_char_prop,
+    overcrowded_hh = overcrowded_hh_prop, hh_wo_sanitation = hh_wo_sanitation_prop,
+    hh_school_absence = hh_school_absence_prop,
+    hh_high_economic_dependence = hh_high_economic_dependence_prop
+  )
+
+processed_path <- path("analysis/data/processed/")
+nodes_filename <- "nodes.csv"
+nodes_filepath <- path(processed_path, nodes_filename)
+
+write_csv(demographics, nodes_filepath, na = "")
